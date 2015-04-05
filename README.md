9a3305ea0e30ad9d4fb4b1144c1b4d25955ca6b1
mh <mh@immerda.ch>
2015-04-05 19:21:13 +0200

    dependencies should either be managed by the package or gem

--- README.md
+++ README.md
@@ -81,21 +81,6 @@ one if you do not use the default yaml setup.
 This class manages the installation of trocla itself. It will not configure
 trocla, it will just install the necessary packages.
 
-### trocla::dependencies
-
-This class is used to install the necessary dependencies if you are not using
-the rubygems module. See dependencies below for more information.
-
-## Dependencies
-
-By default this module requires the rubygems puppet module. If you want to
-use trocla with ruby enterprise, you might be also interested in the
-ruby_enterprise module.
-If the dependencies should be managed internally, set: install_deps to `true`.
-
-You can also use this module with 0 dependencies by setting the option
-use_rubygems to false.
-
 ## Moar
 
 RTFC and for more information about trocla visit: https://github.com/duritong/trocla
--- manifests/master.pp
+++ manifests/master.pp
@@ -2,34 +2,12 @@
 #
 # This module manages the necessary things for trocla on a master.
 #
-# [*install_deps*]: Whether to directly install the necessary dependencies
-# [*use_rubygems*]: Use the rubygems module to manage your dependencies
-# [*provider*]:     Which provider to use to install your dependencies, if you
-#                   don't use the rubygems module
 class trocla::master (
-  $install_deps = true,
-  $use_rubygems = false,
   $provider     = gem,
 ) {
-
-  #Select if the upstream rubygems modules should be required for install
-  if $use_rubygems {
-    require ruby::devel
-    require rubygems::moneta
-    require rubygems::highline
-  }
-
-  #Manually install requirements via gem
-  if $install_deps {
-    class{'trocla::dependencies':
-      provider => $provider,
-    }
-  }
-
   #Main trocla install
   package {'trocla':
     ensure   => present,
     provider => $provider,
   }
-
 }
