203cfefeb0e6bc93eccd7f52ddf8ad846688c1c8
Martin Pfeifer <martin.pfeifer@gmail.com>
2016-04-24 22:37:10 +0200

    refactor custom functions

---
+++ spec/functions/trocla_get_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'trocla_get' do
+  before(:each) do
+    scope.stubs(:lookupvar).with("trocla_configfile").returns('spec/fixtures/troclarc.yaml')
+  end
+
+  it { should run.with_params('key', 'plain').and_return('foo') }
+end
