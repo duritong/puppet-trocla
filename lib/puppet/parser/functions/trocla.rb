70b87a890319b262641503e78495b83df24f20ea
mh <mh@immerda.ch>
2016-06-07 21:59:18 +0200

    whitespace cleanup

--- lib/puppet/parser/functions/trocla.rb
+++ lib/puppet/parser/functions/trocla.rb
@@ -27,7 +27,7 @@ Options can also be passed as a yaml string:
   "
   ) do |*args|
     require File.dirname(__FILE__) + '/../../util/trocla_helper'
-    
+
     Puppet::Util::TroclaHelper.trocla(:password,true,*args)
   end
 end
