7290a8897f1004bae5065bdabb6b17d8e63684f5
fe80 <steffyfort@gmail.com>
2017-08-02 13:37:02 +0200

    Add options for trola_get, fix #23

--- lib/puppet/parser/functions/trocla_get.rb
+++ lib/puppet/parser/functions/trocla_get.rb
@@ -4,7 +4,7 @@ module Puppet::Parser::Functions
 
 Usage:
 
-    $password_user1 = trocla_get(key,[format='plain'[,raise_error=true]])
+    $password_user1 = trocla_get(key,[format='plain'[,options=nill[,raise_error=true]])
 
 Means:
 
@@ -16,10 +16,14 @@ Get the plain text password for the key 'user1'
 
 Get the mysql style sha1 hashed password.
 
+    $cert_x509_key = trocla_get('cert_x509','x509', 'render: keyonly')
+
+Get the x509 style private key.
+
 By default puppet will raise a parse error if the password haven't yet been
 stored in trocla. This can be turned off by setting false as a third argument:
 
-    $password_user3 = trocla_get('user2','mysql',false)
+    $password_user3 = trocla_get('user2','mysql',nill,false)
 
 the return value will be undef if the key & format pair is not found.
 "
@@ -29,8 +33,10 @@ the return value will be undef if the key & format pair is not found.
     end
     require File.dirname(__FILE__) + '/../../util/trocla_helper'
     args[1] ||= 'plain'
-    raise_error = args[2].nil? ? true : args[2]
-    if (answer=Puppet::Util::TroclaHelper.trocla(:get_password,false,[args[0],args[1]])).nil? && raise_error
+    options = args[2] || {}
+    raise_error = args[3].nil? ? true : args[3]
+    raise(Puppet::ParseError, "Trocla parsing ordering change, for disable raise_error use trocla_get('user2','mysql',nill,false)") if (options.is_a?(FalseClass) || options.is_a?(TrueClass))
+    if (answer=Puppet::Util::TroclaHelper.trocla(:get_password,false,[args[0],args[1],options])).nil? && raise_error
       raise(Puppet::ParseError, "No password for key,format #{args[0..1].flatten.inspect} found!")
     end
     answer.nil? ? :undef : answer
