2453cfdc988dfcb0cd4f390bb391c85904c42467
mh <mh@immerda.ch>
2021-01-09 23:46:21 +0100

    support additional characters

--- lib/puppet/functions/trocla/gsub.rb
+++ lib/puppet/functions/trocla/gsub.rb
@@ -9,9 +9,9 @@ Puppet::Functions.create_function(:'trocla::gsub') do
 
   def trocla_gsub(data,options={})
     res = data.dup
-    trocla_keys = res.scan(/%%TROCLA_[\w_\-]+%%/)
+    trocla_keys = res.scan(/%%TROCLA_[\w_\-\.@]+%%/)
     trocla_keys.each do |k|
-      tk = k.match(/%%TROCLA_([\w_\-]+)%%/)[1]
+      tk = k.match(/%%TROCLA_([\w_\-\.@]+)%%/)[1]
       trocla_key = "#{options['prefix']}#{tk}"
       trocla_val = call_function('trocla', trocla_key ,'plain')
       res = res.gsub(k,trocla_val)
